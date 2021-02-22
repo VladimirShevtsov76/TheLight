d1c8bf269ac3049f931c06a9d667c849a1902d24
Vladimir Shevtsov <work2976@gmail.com>
2021-02-22 18:04:17 +0300

    1. Colors change added. 2.Torch function added. 3. Icons added.

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    var isLightOn = true
+    var isLightColor = "black";
     override var prefersStatusBarHidden: Bool {
         return true}
     
@@ -20,14 +21,65 @@ class ViewController: UIViewController {
     }
 
     fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
+        switch  isLightColor {
+        case "black":
+            view.backgroundColor = .black
+        case "white":
+            view.backgroundColor = .white
+        case "green":
+            view.backgroundColor = .green
+        case "yellow":
+            view.backgroundColor = .yellow
+        case "red":
+            view.backgroundColor = .red
+        default:
+            view.backgroundColor = .black
+        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        isLightOn.toggle()
+        switch  isLightColor {
+        case "black":
+            isLightColor = "white"
+        case "white":
+            isLightColor = "green"
+        case "green":
+            isLightColor = "yellow"
+        case "yellow":
+            isLightColor = "red"
+        case "red":
+            isLightColor = "black"
+          default:
+            isLightColor = "black"
+        }
+        
         updateUI()
     }
     
+    func toggleFlash() {
+       
+            guard let device = AVCaptureDevice.default(for: .video), device.hasTorch else { return }
+            
+            do {
+                try device.lockForConfiguration()
+
+                if device.torchMode == .off {
+                    try device.setTorchModeOn(level: AVCaptureDevice.maxAvailableTorchLevel)}
+                else {
+                    device.torchMode = .off
+                        
+                    }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    
+    @IBAction func LightChange(_ sender: Any) {
+        toggleFlash()
+    }
 }
 
